48b7baf582e31a47bfb9be439127dbcfce62c9d7
Stefan Hampejs <stefan.hampejs@gmx.at>
2021-09-14 21:02:26 +0200

    add material modules, add datepicker to day view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,12 @@ import { MatButtonModule } from '@angular/material/button'
 import { MatSidenavModule} from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { MatButtonToggleModule} from '@angular/material/button-toggle';
-import {MatCardModule} from '@angular/material/card'; 
+import { MatCardModule } from '@angular/material/card'; 
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import {FormsModule} from '@angular/forms'
 import { DragDropModule } from '@angular/cdk/drag-drop';
 import { DatePipe } from '@angular/common';
@@ -52,6 +57,10 @@ import { HttpClientModule} from '@angular/common/http'
     MatButtonToggleModule,
     MatSidenavModule,
     MatListModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatFormFieldModule,
+    MatInputModule,
     NgbModule,
     HttpClientModule,
     CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory })
