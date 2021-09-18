425bdbcfde60f0b3f042815088a39bc0afe55266
Stefan Hampejs <stefan.hampejs@gmx.at>
2021-09-18 20:13:21 +0200

    add alerts

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Category } from '../../models/Category';
 import { CategoryService } from '../../services/category-service/category.service';
+import { Alert, ALERTS } from "../../models/Alert";
 
 @Component({
   selector: 'app-category',
@@ -9,6 +10,8 @@ import { CategoryService } from '../../services/category-service/category.servic
   styleUrls: ['./category.component.scss']
 })
 export class CategoryComponent implements OnInit {
+
+  alerts: Alert[] = [];
 
   categories$: Observable<Category[]>;
 
@@ -22,11 +25,16 @@ export class CategoryComponent implements OnInit {
 
   saveCategory() {
     this.categoryService.saveCategory(this.formCategory).subscribe();
-    console.log(this.formCategory.category, this.formCategory.color);
+    this.alerts.push(ALERTS.filter(s => s.type == "success")[0]);
+    //setTimeout(()=>this.alerts.pop(),3000);
   }
 
   editCategory(c: Category) {
     this.formCategory = c;
   }
-  
+
+  close(alert: Alert) {
+    this.alerts.splice(this.alerts.indexOf(alert), 1);
+  }
+
 }
